7684e449515238e2cd8d878f32817a0f809fc234
Andrew Rogers <AndrewRogers@LaserwolveGames.com>
2023-04-04 07:34:30 -0700

    Added webdrivermanager

--- src/constructAutomation/ConstructMethodLibrary.java
+++ src/constructAutomation/ConstructMethodLibrary.java
@@ -11,14 +11,16 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class ConstructMethodLibrary extends ConstructElementMap
 {
-	static WebDriver driver = new EdgeDriver();
+	static WebDriver driver = WebDriverManager.edgedriver().create();
 	static Actions actions = new Actions(driver);
 	
 	public static WebElement clickableElement(By by)
@@ -144,7 +146,6 @@ public class ConstructMethodLibrary extends ConstructElementMap
 
 	public static void start()
 	{
-		System.setProperty(driverKey, driverValue);
 		driver.get(editorURL);
 		click(welcomePopup.noThanksLink);
 	}
--- src/constructAutomation/Data.java
+++ src/constructAutomation/Data.java
@@ -2,8 +2,6 @@ package constructAutomation;
 
 public class Data
 {
-	public static final String driverKey = "webdriver.edge.driver";
-	public static final String driverValue = "C:/tools/msedgedriver.exe";
 	public static final String editorURL = "https://editor.construct.net/";
 	public static final String projectPath = "C:/DaggerQuest";
 	public static final String sourcePath = "C:/assets/output/";
@@ -13,5 +11,4 @@ public class Data
 	{
 		public static final String sprite = "Sprite";
 	}
-	
 }
