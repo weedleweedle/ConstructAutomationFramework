79ecba05b6fa7f83691f14eaa2ca83be8435d80b
Andrew Rogers <AndrewRogers@LaserwolveGames.com>
2023-04-04 07:03:27 -0700

    cleanup

--- src/constructAutomation/Data.java
+++ src/constructAutomation/Data.java
@@ -3,10 +3,10 @@ package constructAutomation;
 public class Data
 {
 	public static final String driverKey = "webdriver.edge.driver";
-	public static final String driverValue = "C:\\driver\\msedgedriver.exe";
+	public static final String driverValue = "C:/tools/msedgedriver.exe";
 	public static final String editorURL = "https://editor.construct.net/";
-	public static final String projectPath = "C:\\DaggerQuest";
-	public static final String sourcePath = "C:\\assets\\output\\";
+	public static final String projectPath = "C:/DaggerQuest";
+	public static final String sourcePath = "C:/assets/output/";
 	public static final String archiveFileExtension = ".zip";
 	
 	public static final class objectTypes
---
+++ src/constructAutomation/SensitiveData.java
@@ -0,0 +1,7 @@
+package constructAutomation;
+
+public class SensitiveData
+{
+	public static final String username = "yourUsername";
+	public static final String password = "yourPassword";
+}
