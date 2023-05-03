3d66ba134eea313e2743191d32f33becc57e4b77
Andrew Rogers <AndrewRogers@LaserwolveGames.com>
2023-05-02 21:50:46 -0700

    Removed extra spaces

--- src/constructAutomation/ConstructMethods.java
+++ src/constructAutomation/ConstructMethods.java
@@ -32,7 +32,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 /**
  * <h1>Construct Method Library</h1> A helper class for common Construct
  * actions.
- * 
+ *
  * @author laserwolve
  */
 class ConstructMethods extends ConstructXpaths {
@@ -49,7 +49,7 @@ class ConstructMethods extends ConstructXpaths {
 	/**
 	 * <h1>Start</h1> Initialize variables and browses to the Construct Editor.
 	 * Launches 'inprivate' to prevent signing into the browser.
-	 * 
+	 *
 	 * @author laserwolve
 	 * @return The current URL, once starting has finished.
 	 */
@@ -82,7 +82,7 @@ class ConstructMethods extends ConstructXpaths {
 	 * <h1>Add Child Element</h1> Creates a new element with the specified
 	 * attributes under the specified element. Locates and returns the newly created
 	 * element with an XPath derived from its tag name and attribute values.
-	 * 
+	 *
 	 * @param by         The locator for the element under which to create the new
 	 *                   element.
 	 * @param tagName    The tag name of the new element.
@@ -109,7 +109,7 @@ class ConstructMethods extends ConstructXpaths {
 
 	/**
 	 * <h1>Click</h1> Clicks the element specified in the By.
-	 * 
+	 *
 	 * @param by The By of the element to click.
 	 * @author laserwolve
 	 * @see {@link org.openqa.selenium.WebElement#click()}
@@ -120,7 +120,7 @@ class ConstructMethods extends ConstructXpaths {
 
 	/**
 	 * <h1>Locate</h1> Turns an XPath string into a <code>By</code> locator.
-	 * 
+	 *
 	 * @param xpath The XPath, as a string.
 	 * @return A <code>By</code> locator based upon the referenced XPath.
 	 */
@@ -131,7 +131,7 @@ class ConstructMethods extends ConstructXpaths {
 	/**
 	 * <h1>Clickable Element</h1> Returns the element specified by the By, after it
 	 * has become clickable.
-	 * 
+	 *
 	 * @param by The By of the element to click.
 	 * @see {@link #clickableElement(By, int)}
 	 * @return The clickable element.
@@ -144,7 +144,7 @@ class ConstructMethods extends ConstructXpaths {
 	/**
 	 * <h1>Clickable Element</h1> Returns the element specified by the By, after it
 	 * has become clickable.
-	 * 
+	 *
 	 * @param by      The locator of the element to click.
 	 * @param seconds The length of time, in seconds, to wait for this element to
 	 *                become clickable.
@@ -158,7 +158,7 @@ class ConstructMethods extends ConstructXpaths {
 	/**
 	 * <h1>Dismiss Welcome Popup</h1> Dismisses the welcome popup, and waits for it
 	 * to disappear.
-	 * 
+	 *
 	 * @author laserwolve
 	 */
 	static void dismissWelcomePopup() {
@@ -169,7 +169,7 @@ class ConstructMethods extends ConstructXpaths {
 
 	/**
 	 * <h1>Element is Present</h1> Determine whether or an element is present.
-	 * 
+	 *
 	 * @param by The locator of the element in question.
 	 * @return Whether or not the element is present.
 	 * @author laserwolve
@@ -181,7 +181,7 @@ class ConstructMethods extends ConstructXpaths {
 	/**
 	 * <h1>Export Project</h1> Exports the currently open project. TODO: Get the
 	 * project name of the open project. Parameterize the export settings.
-	 * 
+	 *
 	 * @param projectName The name of the project that's being exported.
 	 * @throws InterruptedException
 	 */
@@ -249,7 +249,7 @@ class ConstructMethods extends ConstructXpaths {
 
 	/**
 	 * <h1>Is Signed Out</h1> Check whether or not the user signed out.
-	 * 
+	 *
 	 * @return whether or not the user is signed out.
 	 * @author laserwolve
 	 */
@@ -265,7 +265,7 @@ class ConstructMethods extends ConstructXpaths {
 
 	/**
 	 * <h1>Check Expiry</h1> Check if the expiry time has been exceeded.
-	 * 
+	 *
 	 * @return whether or not the current time is past the expiry time.
 	 * @author laserwolve
 	 */
@@ -276,7 +276,7 @@ class ConstructMethods extends ConstructXpaths {
 	/**
 	 * <h1>Set Expiry Time</h1> Set the time that, when exceeded, will indicate a
 	 * process has exceeded its normal boundaries.
-	 * 
+	 *
 	 * @param seconds The number of seconds to set.
 	 * @author laserwolve
 	 */
@@ -287,7 +287,7 @@ class ConstructMethods extends ConstructXpaths {
 	/**
 	 * <h1>Exceeded Expiry Time</h1> Throws a {@link TimeoutException}, with a
 	 * message.
-	 * 
+	 *
 	 * @param message The message to include with the exception.
 	 * @author laserwolve
 	 */
@@ -297,7 +297,7 @@ class ConstructMethods extends ConstructXpaths {
 
 	/**
 	 * <h1>Log In</h1> Logs in to the Construct Editor.
-	 * 
+	 *
 	 * @author laserwolve
 	 */
 	static void logIn() {
@@ -327,7 +327,7 @@ class ConstructMethods extends ConstructXpaths {
 	 * of time it takes a project to load is determined both by the project's size
 	 * and speed of the computer. Uses keyboard commands to interact with the
 	 * Chromium "Let site edit files?" popup.
-	 * 
+	 *
 	 * @param projectName A string which is contained by the name of the project to
 	 *                    open.
 	 * @author laserwolve
@@ -362,7 +362,7 @@ class ConstructMethods extends ConstructXpaths {
 	/**
 	 * <h1>Present Element</h1> Gets the element specified, as long as it is present
 	 * with the time limit specified.
-	 * 
+	 *
 	 * @param by      The locator of the element to find.
 	 * @param Seconds The number of seconds to wait for the element to be present.
 	 * @return The present element, or null if it can't be located.
@@ -374,7 +374,7 @@ class ConstructMethods extends ConstructXpaths {
 
 	/**
 	 * <h1>Quit</h1> Quits the script.
-	 * 
+	 *
 	 * @author laserwolve
 	 * @see {@link org.openqa.selenium.WebDriver#quit()}
 	 */
@@ -385,7 +385,7 @@ class ConstructMethods extends ConstructXpaths {
 	/**
 	 * <h1>Scroll to Element</h1> Scrolls to the specified element, using
 	 * JavaScript.
-	 * 
+	 *
 	 * @param by The locator of the element to scroll to.
 	 * @author laserwolve
 	 */
@@ -395,7 +395,7 @@ class ConstructMethods extends ConstructXpaths {
 
 	/**
 	 * <h1>Send Text</h1> Sends the specified text to specified element.
-	 * 
+	 *
 	 * @param by   The locator of the element which will recieve the text.
 	 * @param text The text to be inputted.
 	 * @author laserwolve
@@ -410,7 +410,7 @@ class ConstructMethods extends ConstructXpaths {
 
 	/**
 	 * <h1>Stop</h1> Returns a wait of the specified duration in seconds.
-	 * 
+	 *
 	 * @param seconds The duration (in seconds) of this wait.
 	 * @return A wait of the specified duration.
 	 * @author laserwolve
@@ -422,7 +422,7 @@ class ConstructMethods extends ConstructXpaths {
 
 	/**
 	 * <h1>Switch to Default Content</h1> Switches to the default content.
-	 * 
+	 *
 	 * @see {@link org.openqa.selenium.WebDriver.TargetLocator#defaultContent()}
 	 * @author laserwolve
 	 * @return The WebDriver focused on the default content.
@@ -433,7 +433,7 @@ class ConstructMethods extends ConstructXpaths {
 
 	/**
 	 * <h1>Switch to IFrame</h1> Switches to the specified IFrame.
-	 * 
+	 *
 	 * @param by The IFrame to switch to.
 	 * @author laserwolve
 	 * @return The driver focused on the iframe.
@@ -448,7 +448,7 @@ class ConstructMethods extends ConstructXpaths {
 	 * you can't do other things on the computer executing this method. This method
 	 * sets the clipboard's contents to <strong>path</strong>, then pastes it into
 	 * the File Explorer window.
-	 * 
+	 *
 	 * @param path The file path to type/paste into the File Explorer window.
 	 * @throws InterruptedException from {@link Thread#sleep}
 	 * @author laserwolve
@@ -469,7 +469,7 @@ class ConstructMethods extends ConstructXpaths {
 	/**
 	 * <h1>Wait Until Element is Gone</h1> Waits until the specified element is no
 	 * longer visible.
-	 * 
+	 *
 	 * @param By The element for which to wait upon.
 	 * @see {@link #waitUntilElementIsGone(by, int)}
 	 * @author laserwolve
@@ -481,7 +481,7 @@ class ConstructMethods extends ConstructXpaths {
 	/**
 	 * <h1>Wait Until Element is Gone</h1> Waits until the specified element is no
 	 * longer visible.
-	 * 
+	 *
 	 * @param The     element for which to wait upon.
 	 * @param seconds How long to wait, in seconds, for the element to no longer be
 	 *                visible.
@@ -500,7 +500,7 @@ class ConstructMethods extends ConstructXpaths {
 	/**
 	 * <h1>Wait Until Element is Visible</h1> Waits until the specified element is
 	 * visible.
-	 * 
+	 *
 	 * @param The     element for which to wait upon.
 	 * @param seconds How long to wait, in seconds, for the element to be visible.
 	 * @author laserwolve
@@ -513,7 +513,7 @@ class ConstructMethods extends ConstructXpaths {
 	/**
 	 * <h1>Wait Until Text is</h1> Wait until the text of an element is the
 	 * specified text.
-	 * 
+	 *
 	 * @param by      The <code>By</code> of the element of which to check the text
 	 * @param text    The awaited text
 	 * @param seconds How long to wait for the text
@@ -528,7 +528,7 @@ class ConstructMethods extends ConstructXpaths {
 	/**
 	 * <h1>Context Click</h1> Performs a context click on the element specified in
 	 * the By.
-	 * 
+	 *
 	 * @param by The By of the element on which to perform a context click.
 	 * @author laserwolve
 	 * @see {@link org.openqa.selenium.interactions.Actions#contextClick(WebElement)}
@@ -540,7 +540,7 @@ class ConstructMethods extends ConstructXpaths {
 	/**
 	 * <h1>Context Click Upper Left Corner</h1> Performs a context click that's 1
 	 * pixel down and to the right of the specified element.
-	 * 
+	 *
 	 * @param by The element on which to perform the context click.
 	 * @author laserwolve
 	 */
@@ -554,7 +554,7 @@ class ConstructMethods extends ConstructXpaths {
 
 	/**
 	 * <h1>Double Click</h1> Double clicks the element specified in the locator.
-	 * 
+	 *
 	 * @param by The locator of the element to double click.
 	 * @author laserwolve
 	 * @see {@link org.openqa.selenium.interactions.Actions#doubleClick(WebElement)}
@@ -566,7 +566,7 @@ class ConstructMethods extends ConstructXpaths {
 	/**
 	 * <h1>Is Element Clickable</h1> Uses {@link #clickableElement(By)} to determine
 	 * if an element is clickable or not.
-	 * 
+	 *
 	 * @param by The locator of the element to check.
 	 * @return whether or not the element is clickable.
 	 * @author laserwolve
@@ -581,7 +581,7 @@ class ConstructMethods extends ConstructXpaths {
 	 * the Start page. The amount of time a project takes to load is determined both
 	 * by the project's size and speed of the computer. Uses keyboard commands to
 	 * interact with the Chromium "Let site edit files?" popup.
-	 * 
+	 *
 	 * @param MaximumProjectLoadTimeInSeconds The maximum amount of time to wait (in
 	 *                                        seconds) for the project to load.
 	 * @throws InterruptedException in {@link #typeIntoFileExplorer}
@@ -610,7 +610,7 @@ class ConstructMethods extends ConstructXpaths {
 
 	/**
 	 * <h1>Refresh</h1> Refreshes the webpage.
-	 * 
+	 *
 	 * @author laserwolve
 	 * @see {@link org.openqa.selenium.WebDriver.Navigation#refresh()}
 	 */
@@ -620,7 +620,7 @@ class ConstructMethods extends ConstructXpaths {
 
 	/**
 	 * <h1>Stop</h1> Returns a wait of a fixed duration.
-	 * 
+	 *
 	 * @see {@link #stop(int)}
 	 * @return A wait of the specified duration.
 	 * @author laserwolve
@@ -632,7 +632,7 @@ class ConstructMethods extends ConstructXpaths {
 	/**
 	 * <h1>Wait for Element to have Attribute</h1> Waits a specified amount of
 	 * seconds for the specified attribute to appear in the specified element.
-	 * 
+	 *
 	 * @param xpath     The locator of the element to wait upon.
 	 * @param seconds   How many seconds to wait.
 	 * @param attribute Waits for this attribute to appear.
@@ -646,7 +646,7 @@ class ConstructMethods extends ConstructXpaths {
 	/**
 	 * <h1>Wait for Element to not have Attribute</h1> Waits a specified amount of
 	 * seconds for the specified attribute to not be in the specified element.
-	 * 
+	 *
 	 * @param xpath     The locator of the element to wait upon.
 	 * @param seconds   How many seconds to wait.
 	 * @param attribute Waits for this attribute to be gone.
@@ -664,7 +664,7 @@ class ConstructMethods extends ConstructXpaths {
 	 * sprite already exists. Otherwise creates a new sprite. Corrects the Y value
 	 * of the new animations to the specified value, then crops to visible pixels.
 	 * Saves the project.
-	 * 
+	 *
 	 * @param path          The path to the zipped archive containing the
 	 *                      animations.
 	 * @param projectFolder The path to the project folder.
@@ -676,7 +676,7 @@ class ConstructMethods extends ConstructXpaths {
 		openProjectFolder(6000, projectFolder);
 
 		sendText(Project.ProjectBar.searchBar, sprite);
-		
+
 		// TODO: Check if the sprite doesn't exist here. If it doesn't, create it.
 
 		contextClick(Project.ProjectBar.searchResult(sprite));
@@ -763,10 +763,10 @@ class ConstructMethods extends ConstructXpaths {
 	 * exist, this method will create "Animation 3".) Must be in the Animations
 	 * Editor. Waits until the animatiom is created to continue. This deselects all
 	 * currently selected animations and selects the newly created animation.
-	 * 
+	 *
 	 * @author laserwolve
 	 * @return The name of the animation that was created.
-	 * 
+	 *
 	 */
 	static String addAnimation() {
 
@@ -788,7 +788,7 @@ class ConstructMethods extends ConstructXpaths {
 	/**
 	 * <h1>Number of Animations</h1> Gets how many animations this sprite has. Must
 	 * be in the Animations Editor.
-	 * 
+	 *
 	 * @return The number of animations of the sprite.
 	 * @author laserwolve
 	 */
